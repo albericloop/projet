ad44a35678d6c3aeef99e4ac4fa5ff3b51c1d618
Roch <rochfolly@hotmail.fr>
2018-10-15 11:50:42 +0200

    Base tableau

---
+++ src/Tableau/Tableau.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Tableau extends Component {
+  constructor(props){
+    super(props);
+    this.hauteur = null;
+}
+
+render(){
+  return(
+    <table>
+
+    </table>
+  )
+}
+
+export default Tableau;
